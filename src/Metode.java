cc22b395ace80052d8ed8928dead55260fb4c364
Syber55 <sajib_causevic@hotmail.com>
2018-02-09 14:31:33 +0100

    Verzija 0.03

--- src/Metode.java
+++ src/Metode.java
@@ -1,15 +1,15 @@
 
 public class Metode {
 	public static int[] eliminateDuplicates(int[] list) {
-		int [] niz = list;
+		int[] niz = list;
 		return niz;
 	}
-	
+
 	public static boolean isSorted(int[] list) {
 		boolean provjera = false;
 		return provjera;
 	}
-	
+
 	public static String sort(String s) {
 		String sortirano = "";
 		return sortirano;
@@ -19,13 +19,16 @@ public class Metode {
 		double suma = 0;
 		return suma;
 	}
-	
+
 	public static double sumColumn(double[][] m, int columnIndex) {
 		double sumaKolone = 0;
+		for (int i = 0; i < m.length && i < m[0].length; i++) {
+			sumaKolone += m[i][i];
+		}
 		return sumaKolone;
 	}
-	
+
 	public static void sort(int m[][]) {
-		
+
 	}
 }
