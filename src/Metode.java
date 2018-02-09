edc9f28fb707016225828f2cdeca2ebf472d8e23
Estela Ramic <estelaramic@gmail.com>
2018-02-09 17:06:56 +0100

    implementirane metode sumMajorDiagonal  i sort(String)

--- src/Metode.java
+++ src/Metode.java
@@ -10,13 +10,33 @@ public class Metode {
 		return provjera;
 	}
 
+	//Write a method that returns a sorted string using the following header: public static String sort(String s) For example, sort("acb") returns abc.
 	public static String sort(String s) {
 		String sortirano = "";
+		char[] niz = new char[s.length()];
+		int i;
+		for (i = 0; i < niz.length; i++) {
+			niz[i] = s.charAt(i);
+		}
+		java.util.Arrays.sort(niz);
+		//System.out.println(niz);
+		//String novi_string = java.util.Arrays.toString(niz);
+		s="";
+		for (i = 0; i < niz.length; i++) {
+			s += niz[i];
+		}
+					
+		sortirano=s;
 		return sortirano;
 	}
 
 	public static double sumMajorDiagonal(double[][] m) {
 		double suma = 0;
+		int n=m.length;
+		int i;
+		for(i=0; i<n; i++){
+			suma += m[i][i];
+		}
 		return suma;
 	}
 
